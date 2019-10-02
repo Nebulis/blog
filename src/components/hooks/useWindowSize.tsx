1f0c750eef02085b01d75651244b61e2474d72dd
Nebulis <nebounet@gmail.com>
2019-10-02 22:16:14 +0800

    feat: banner

---
+++ src/components/hooks/useWindowSize.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+
+let defaultHeight: number
+let defaultWidth: number
+
+if (typeof window !== `undefined`) {
+  defaultHeight = window.innerHeight
+  defaultWidth = window.innerWidth
+}
+
+export const useWindowSize = () => {
+  const [dimensions, setDimensions] = useState({
+    windowHeight: defaultHeight,
+    windowWidth: defaultWidth,
+  })
+
+  useEffect(() => {
+    const handler = () =>
+      setDimensions({
+        windowHeight: window.innerHeight,
+        windowWidth: window.innerWidth,
+      })
+
+    window.addEventListener(`resize`, handler)
+    return () => window.removeEventListener(`resize`, handler)
+  }, [])
+
+  return dimensions
+}
