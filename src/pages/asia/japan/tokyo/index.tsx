623686b6b0dae8f0f32ed254abb8126ff59843c7
Nebulis <nebounet@gmail.com>
2019-10-14 23:31:51 +0800

    feat: add japan home page

---
+++ src/pages/asia/japan/tokyo/index.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { Link } from "gatsby"
+import { Header } from "../../../../components/layout/header"
+import { Maintenance } from "../../../../components/layout/maintenance"
+import SEO from "../../../../components/layout/seo"
+
+const IndexPage = () => {
+  return (
+    <>
+      <SEO title="main" />
+      <Maintenance>{typeof window !== `undefined` ? <Content /> : null}</Maintenance>
+    </>
+  )
+}
+
+const Content = () => {
+  return (
+    <>
+      <Header />
+      Tokyo page
+      <Link to="/asia/japan">Japon</Link>
+    </>
+  )
+}
+
+export default IndexPage
