6b8c5da80fd1355b6d48e2b97e80436866bb9711
Nebulis <nebounet@gmail.com>
2019-10-17 21:07:51 +0800

    chore: use JapanCard component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,15 @@
 import React, { FunctionComponent } from "react"
 import { css } from "@emotion/core"
 import styled from "@emotion/styled"
+import { Link } from "gatsby"
 
 interface CardProps {
   title?: string
   className?: string
+  to: string
 }
 
-export const Card: FunctionComponent<CardProps> = ({ children, title, className }) => {
+export const Card: FunctionComponent<CardProps> = ({ children, title, className, to }) => {
   if (!children || !Array.isArray(children) || children.length < 2 || !React.isValidElement(children[1]))
     throw new Error("I need 2 children")
   return (
@@ -33,7 +35,9 @@ export const Card: FunctionComponent<CardProps> = ({ children, title, className 
       <h4 className="normal tc i b">{title}</h4>
       <div className="image mb3">{children[0]}</div>
       {React.cloneElement(children[1], { className: `${children[1].props.className || ""} text` })}
-      <h5 className="normal tc ttu next">Lire La suite</h5>
+      <h5 className="normal tc ttu next">
+        <Link to={to}>Lire La suite</Link>
+      </h5>
     </div>
   )
 }
