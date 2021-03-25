3086346bdb56b65f1e92de904b4e5f2ee7bcd16e
Nebulis <nebounet@gmail.com>
2021-03-26 01:05:38 +0800

    fix: enable netlify cache on fonts

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,18 +15,23 @@ exports.onCreatePage = ({ page, actions }) => {
   createPage(newPage)
   // }
 }
+exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
+  const PATTERN = /\.(eot|otf|ttf|woff(2)?)(\?.*)?$/
 
-exports.onCreateWebpackConfig = ({ loaders, actions }) => {
-  const urlLoader = loaders.url()
-  urlLoader.options.name = `static/fonts/[name]-[hash].[ext]`
-  actions.setWebpackConfig({
+  const config = getConfig()
+  const newConfig = {
+    ...config,
     module: {
-      rules: [
-        {
-          use: [urlLoader],
-          test: /\.(eot|otf|ttf|woff(2)?)(\?.*)?$/,
-        },
-      ],
+      // Manually swap out the rule who's test matches PATTERN
+      rules: config.module.rules.map((rule) => {
+        if (rule.test && rule.test.toString() === PATTERN.toString()) {
+          rule.use[0].options.limit = 1
+          rule.use[0].options.name = `static/fonts/[name]-[hash].[ext]`
+        }
+        return rule
+      }),
     },
-  })
+  }
+
+  actions.replaceWebpackConfig(newConfig)
 }
