15fe0e432d53ab01c20436f1589cc90d6b37d96d
Nebulis <nebounet@gmail.com>
2021-08-06 00:19:53 +0800

    feat: summer page

---
+++ src/pages/organisation/when-to-go/summer.tsx
@@ -0,0 +1,71 @@
+import React, { useContext, useState } from "react"
+import SEO from "../../../components/layout/seo"
+import { MainTitleSection, SectionContent } from "../../../components/core/section"
+import { useCustomTranslation } from "../../../i18n-hook"
+import i18n from "i18next"
+import translationFr from "../../../locales/fr/organisation/when-to-go/summer.json"
+import translationEn from "../../../locales/en/organisation/when-to-go/summer.json"
+import { Divider } from "../../../components/core/divider"
+import { PageQuote } from "../../../components/core/quote"
+import { PageProps } from "gatsby"
+import { CountriesContainer, CountryContainer } from "../../../components/layout/organisation-layout"
+import { PrimaryBlogLayoutWithDrawer } from "../../../components/layout/main-layout"
+import { getArticles, getContinentsFromCountries, sortByLabel } from "../../../components/core/links/links.utils"
+import { ApplicationContext } from "../../../components/application"
+import { SelectByContinent } from "../../../components/core/select"
+import { getImageProps, getSharedImages } from "../../../components/core/discovery"
+
+const namespace = "organisation/when-to-go/summer"
+i18n.addResourceBundle("fr", namespace, translationFr)
+i18n.addResourceBundle("en", namespace, translationEn)
+const IndexPage: React.FunctionComponent<PageProps> = ({ location }) => {
+  const { development } = useContext(ApplicationContext)
+  const { t, i18n } = useCustomTranslation([namespace, "common"])
+  const [selectedContinent, setSelectedContinent] = useState<string>()
+  const socialNetworkDescription = `${t("part2")} ${t("part3")}`
+  const countries = getArticles({
+    kind: "country",
+    card: false,
+    development,
+    seasons: ["summer"],
+  })
+  const continents = getContinentsFromCountries(countries)
+  return (
+    <>
+      <SEO
+        title={t("title")}
+        fullTitle={t("full-title")}
+        location={location}
+        socialNetworkDescription={socialNetworkDescription}
+        googleDescription={t("google-description")}
+      />
+      <PrimaryBlogLayoutWithDrawer page="summer" location={location}>
+        <MainTitleSection>{t("title")}</MainTitleSection>
+        <Divider />
+        <SectionContent>
+          <PageQuote>{t("part1")}</PageQuote>
+          <PageQuote position="none">{t("part2")}</PageQuote>
+          <PageQuote position="none">{t("part3")}</PageQuote>
+        </SectionContent>
+        <Divider />
+        <SelectByContinent continents={continents} onChange={(value) => setSelectedContinent(value)} />
+        <CountriesContainer>
+          {countries
+            .filter((country) => (selectedContinent ? country.tags.includes(selectedContinent) : true))
+            .sort(sortByLabel(i18n.languageCode))
+            .map(({ label, imageProps, id, tags }, index) => (
+              <CountryContainer
+                title={label[i18n.languageCode]}
+                image={getSharedImages(tags)}
+                to={id}
+                key={index}
+                imageProps={{ image: imageProps?.image, ...getImageProps(id) }}
+              />
+            ))}
+        </CountriesContainer>
+      </PrimaryBlogLayoutWithDrawer>
+    </>
+  )
+}
+
+export default IndexPage
