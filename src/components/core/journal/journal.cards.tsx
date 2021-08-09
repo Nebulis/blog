97de3cc76d2918feb907fde3b8a0950987f1431a
Nebulis <nebounet@gmail.com>
2021-08-09 19:41:17 +0800

    fix: use card component on living abroad articles, instead of japan card

--- src/components/core/journal/journal.cards.tsx
+++ src/components/core/journal/journal.cards.tsx
@@ -2,25 +2,25 @@ import { ExtraCardProps } from "../../../types/shared"
 import { useCustomTranslation } from "../../../i18n-hook"
 import commonFr from "../../../locales/fr/common.json"
 import commonEn from "../../../locales/en/common.json"
-import { JapanCard } from "../japan/japan.cards"
 import React, { FunctionComponent } from "react"
 import { SharedJournalImages } from "../../images/journal/shared-journal-images"
+import { Card } from "../../card"
 
 export const MovingAbroadWhyCard: FunctionComponent<ExtraCardProps> = ({ fluidObject, card = {} }) => {
   const { i18n } = useCustomTranslation()
   const common = i18n.languageCode === "fr" ? commonFr : commonEn
   return (
-    <JapanCard title={common.journal["living-abroad"].card["moving-abroad-why"]} to="moving-abroad-why" {...card}>
+    <Card title={common.journal["living-abroad"].card["moving-abroad-why"]} to="moving-abroad-why" {...card}>
       <SharedJournalImages image="movingAbroadWhy" fluidObject={fluidObject} />
-    </JapanCard>
+    </Card>
   )
 }
 export const LivingAbroadWhereCard: FunctionComponent<ExtraCardProps> = ({ fluidObject, card = {} }) => {
   const { i18n } = useCustomTranslation()
   const common = i18n.languageCode === "fr" ? commonFr : commonEn
   return (
-    <JapanCard title={common.journal["living-abroad"].card["living-abroad-where"]} to="living-abroad-where" {...card}>
+    <Card title={common.journal["living-abroad"].card["living-abroad-where"]} to="living-abroad-where" {...card}>
       <SharedJournalImages image="livingAbroadWhere" fluidObject={fluidObject} />
-    </JapanCard>
+    </Card>
   )
 }
