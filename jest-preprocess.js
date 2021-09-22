f63ea8f305ba94f1b9ce193386a91f3a2ec1f359
Nebulis <nebounet@gmail.com>
2021-09-22 12:42:12 +0800

    chore: update to jest 27

--- jest-preprocess.js
+++ jest-preprocess.js
@@ -2,4 +2,4 @@ const babelOptions = {
   presets: ["babel-preset-gatsby", "@babel/preset-typescript", "@emotion/babel-preset-css-prop"],
 }
 
-module.exports = require("babel-jest").createTransformer(babelOptions)
+module.exports = require("babel-jest").default.createTransformer(babelOptions)
